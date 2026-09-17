ca7f8fb076c7a668c008596b2509bd4dcd9e333f
Kalieri <88911209+Musdkar@users.noreply.github.com>
2026-09-17 16:28:41 +0800

    test: fix client loader id regression check

--- test/client-loader.test.js
+++ test/client-loader.test.js
@@ -6,7 +6,7 @@ const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url
 const client = await readFile(new URL('../lib/client.js', import.meta.url), 'utf8')
 
 test('client bundle registers under the package name', () => {
-  const match = /__ModuleLoader__\.load\(\{\s*id:\s*['\"]([^'\"]+)['\"]/u.exec(client)
+  const match = /__ModuleLoader__\.load\(\{\s*id:\s*['"]([^'"]+)['"]/u.exec(client)
   assert.ok(match, 'client bundle must register itself through __ModuleLoader__.load')
   assert.equal(match[1], pkg.name)
 })
